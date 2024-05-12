9dbb6af441be3c734e8d6c6605b0566b7594b5cf
owy <126146587+goldietiara@users.noreply.github.com>
2024-05-12 22:15:13 +0700

    style: landing page background and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
-import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>henlo</h1>
+      <div className="absolute p-10 w-full right-0 left-0">
+        <div className="flex justify-center">
+          <h1 className=" bg-pink-500 p-10 text-3xl w-fit">henlo</h1>
+        </div>
+      </div>
+
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="grid grid-cols-12 w-full h-full border-sky-200 border">
+          {Array.from({ length: 12 * 100 }, (_, i) => (
+            <div
+              key={i}
+              className="border-sky-200 border w-full h-10 sm:h-14 md:h-16 lg:h-20"
+            />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
--- components/layout/nav-bar.tsx
+++ components/layout/nav-bar.tsx
@@ -7,15 +7,15 @@ const NavBar = () => {
     <nav className=" bg-transparent py-5 px-16 h-fit w-full flex justify-between items-center">
       <h1 className=" text-xl font-mono text-gray-700">Goldie Portfolio</h1>
       <div className="flex text-white font-mono text-sm">
-        <p className=" bg-yellow-400 py-2 px-5 rotate-0 hover:animate-wiggle hover:rotate-12  duration-150 ease-in-out transition-all cursor-pointer">
-          Work
-        </p>
-        <p className=" bg-sky-300 py-2 px-5 rotate-0 hover:animate-wiggle2 hover:-rotate-12 duration-150 ease-in-out transition-all cursor-pointer">
-          About
-        </p>
-        <p className=" bg-sky-400 py-2 px-5 rotate-0 hover:animate-wiggle hover:rotate-12 duration-150 ease-in-out transition-all cursor-pointer">
-          Contact
-        </p>
+        <button className=" bg-yellow-400 py-2 px-5 rotate-0 hover:animate-wiggle hover:rotate-12  duration-150 ease-in-out transition-all cursor-pointer">
+          <p>Work</p>
+        </button>
+        <button className=" bg-sky-300 py-2 px-5 rotate-0 hover:animate-wiggle2 hover:-rotate-12 duration-150 ease-in-out transition-all cursor-pointer">
+          <p>About</p>
+        </button>
+        <button className=" bg-sky-400 py-2 px-5 rotate-0 hover:animate-wiggle hover:rotate-12 duration-150 ease-in-out transition-all cursor-pointer">
+          <p>Contact</p>
+        </button>
       </div>
     </nav>
   );
