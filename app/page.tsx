305637fa741085faa40ca271dabc147cffc9fda4
owy <126146587+goldietiara@users.noreply.github.com>
2024-05-14 12:28:45 +0700

    contact

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AboutMe from "@/components/content/about-me";
 import Work from "@/components/content/work";
+import Contact from "@/components/content/contact";
 
 export default function Home() {
   return (
@@ -8,12 +9,13 @@ export default function Home() {
         <div className="flex flex-col items-center gap-10">
           <AboutMe />
           <Work />
+          <Contact />
         </div>
       </div>
 
       <div className="w-full h-full flex justify-center items-center opacity-50 md:opacity-100 z-0">
         <div className="grid grid-cols-12 w-full h-full border-sky-200 border-[0.5px]">
-          {Array.from({ length: 12 * 100 }, (_, i) => (
+          {Array.from({ length: 12 * 30 }, (_, i) => (
             <div
               key={i}
               className="border-sky-200 border-[0.5px] w-full h-8 sm:h-14 md:h-16 lg:h-20"
