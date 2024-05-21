509bb23bfa448d8ef52e7c05459fa16459054a22
owy <126146587+goldietiara@users.noreply.github.com>
2024-05-21 12:02:12 +0700

    fix animation on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,34 @@
+"use client";
 import AboutMe from "@/components/content/about-me";
 import Work from "@/components/content/work";
 import Contact from "@/components/content/contact";
 import Intro from "@/components/content/intro";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <main>
       <div className="absolute p-10 w-full right-0 left-0 z-10 top-[100px] md:top-0">
         <div className="flex flex-col items-center mb-24">
-          <section id="intro" className=" mb=0 md:mb-36">
+          <section id="intro">
             <Intro />
           </section>
-          <section id="work" className=" z-40">
-            <Work />
-          </section>
-          <section id="about">
-            <AboutMe />
-          </section>
-          <section id="contact">
-            <Contact />
-          </section>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 1.3 }}
+            className="flex flex-col items-center"
+          >
+            <section id="work" className=" z-40">
+              <Work />
+            </section>
+            <section id="about">
+              <AboutMe />
+            </section>
+            <section id="contact">
+              <Contact />
+            </section>
+          </motion.div>
         </div>
       </div>
 
